0023ba6b8f4546679d24b093b1b0eead61c3273a
Iago <iagobrazmendes@gmail.com>
2021-01-01 16:45:17 -0300

    first commit

---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import {createGlobalStyle} from "styled-components";
+
+export default createGlobalStyle`
+	:root
+	{
+		font-size: 10px;
+	}
+	*
+	{
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+	body
+	{
+		
+	}
+	#__next
+	{
+		
+	}
+`
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme =
+{
+  colors:
+  {
+    
+  }
+}
+
+export default theme
