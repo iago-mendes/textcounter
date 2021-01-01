0e95e72a14fb01c04504047da4698b0ca13a2892
Iago <iagobrazmendes@gmail.com>
2021-01-01 17:30:33 -0300

    add dark theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,12 @@ const theme =
 {
   colors:
   {
-    
+    primary: '#282a36',
+		secondary: '#44475a',
+		secondaryStrong: '#6272a4',
+		text: '#f8f8f2',
+		textarea: '#bd93f9',
+		textareaFocus: '#ff79c6'
   }
 }
 
