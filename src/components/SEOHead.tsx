82878102acb9f75f5f54562e84a2c0e26610b0b9
Iago <iagobrazmendes@gmail.com>
2021-02-17 22:34:59 -0300

    update seo

--- src/components/SEOHead.tsx
+++ src/components/SEOHead.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head"
+import Head from 'next/head'
 
 interface SEOHeadProps
 {
@@ -7,18 +7,41 @@ interface SEOHeadProps
 
 const SEOHead: React.FC<SEOHeadProps> = ({isDark}) =>
 {
+	const meta =
+	{
+		title: 'Text Counter | Online Tool to Count Text Elements',
+		description: 'Text Counter is a free online tool to count words, characters, paragraphs, letter density, and more, making the process of writing faster and more precise.',
+		image: 'https://textcounter.tools/thumbnail.png',
+		url: 'https://textcounter.tools'
+	}
+
 	return (
 		<Head>
 			<link rel='icon' href={isDark ? '/faviconDark.svg' : '/faviconLight.svg'} />
-
-			<title>Text Counter | Online Tool to Count Text Elements</title>
-			<meta name="description" content="Text Counter is a free online tool to count words, characters, paragraphs, letter density, and more, making the process of writing faster and more precise." />
-			<meta name="keywords" content="Text, Word, Character, Paragraph, Letter, Counter, Density" />
-			<meta name="author" content="Iago Mendes" />
-
-			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<meta name='viewport' content='width=device-width, initial-scale=1' />
 			<meta charSet='utf-8' />
 			<meta name='robots' content='index, follow' />
+
+			<meta name='keywords' content='Text, Word, Character, Paragraph, Letter, Counter, Density' />
+			<meta name='author' content='Iago Mendes' />
+
+			<title>{meta.title}</title>
+			<meta name='title' content={meta.title} />
+			<meta name='description' content={meta.description} />
+
+			<meta property='og:type' content='article' />
+			<meta property='og:url' content={meta.url} />
+			<meta property='og:title' content={meta.title} />
+			<meta property='og:description' content={meta.description} />
+			<meta property='og:image' content={meta.image} />
+			<meta property='og:site_name' content='Text Counter' />
+
+			<meta property='twitter:card' content='summary_large_image' />
+			<meta property='twitter:url' content={meta.url} />
+			<meta property='twitter:title' content={meta.title} />
+			<meta property='twitter:description' content={meta.description} />
+			<meta property='twitter:image' content={meta.image} />
+			<meta name='twitter:creator' content='@iago_mendes' />
 		</Head>
 	)
 }
